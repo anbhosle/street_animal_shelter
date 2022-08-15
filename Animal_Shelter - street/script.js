891ded55b68d39f87b8f04bac3934be046c6a2fd
anbhosle <73429979+anbhosle@users.noreply.github.com>
2022-08-15 11:51:47 +0530

    Update script.js

--- Animal_Shelter - street/script.js
+++ Animal_Shelter - street/script.js
@@ -1,12 +1,1 @@
-$(document).ready(function(){
 
-    $('.menu i').click(function(){
-        $(this).toggleclass('fa-times');
-        $('header').toggleclass('toggle');
-    });
-
-    $(window).on('scroll load', function() {
-        $('.menu i').removeClass('fa-times');
-        $('header').removeClass('toggle');
-    });
-});  
